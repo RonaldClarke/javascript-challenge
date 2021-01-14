1c8e838d89c80fdca6e0ac3542695016339955c9
Ronald Clarke <ronnie.clarke@utoronto.ca>
2021-01-14 13:49:51 -0500

    level 2 progress

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -3,9 +3,12 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
-var inputFilterDate = d3.select("#datetime")
-var form = d3.select("form")
+var dateButton = d3.select("#filter-btn-date");
+var inputFilterDate = d3.select("#datetime");
+var dateForm = d3.select("date-form");
+var cityButton = d3.select("#filter-btn-city");
+var inputFilterCity = d3.select("#cityname");
+var cityForm = d3.select("city-form")
 data.forEach((UFOReport) => {
     var row = tbody.append("tr");
     Object.entries(UFOReport).forEach(([key, value]) => {
@@ -13,11 +16,14 @@ data.forEach((UFOReport) => {
         cell.text(value);
     });
 });
-button.on("click", runFilter);
-form.on("submit", runFilter);
+dateButton.on("click", runFilter);
+dateForm.on("submit", runFilter);
+cityButton.on("click", runFilter);
+cityForm.on("submit", runFilter);
 function runFilter() {
     d3.event.preventDefault();
     var inputDate = inputFilterDate.property("value");
+    var inputCity = inputFilterCity.property("value");
     var FilteredData = tableData.filter(sighting => sighting.datetime === inputDate);
     tbody.html("");
     FilteredData.forEach((UFOReport) => {
