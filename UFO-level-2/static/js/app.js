c5f79054c86d2857c9fbf66b2f2853bd47234e7d
Ronald Clarke <ronnie.clarke@utoronto.ca>
2021-01-14 14:29:47 -0500

    adding filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -8,7 +8,10 @@ var inputFilterDate = d3.select("#datetime");
 var dateForm = d3.select("date-form");
 var cityButton = d3.select("#filter-btn-city");
 var inputFilterCity = d3.select("#cityname");
-var cityForm = d3.select("city-form")
+var cityForm = d3.select("city-form");
+var stateButton = d3.select("#filter-btn-state");
+var inputFilterState = d3.select("#statename");
+var stateForm = d3.select("state-form");
 data.forEach((UFOReport) => {
     var row = tbody.append("tr");
     Object.entries(UFOReport).forEach(([key, value]) => {
@@ -20,15 +23,26 @@ dateButton.on("click", runFilter);
 dateForm.on("submit", runFilter);
 cityButton.on("click", runFilter);
 cityForm.on("submit", runFilter);
+stateButton.on("click", runFilter);
+stateForm.on("submit", runFilter);
 function runFilter() {
     d3.event.preventDefault();
+    tbody.html("");
     var inputDate = inputFilterDate.property("value");
     var inputCity = inputFilterCity.property("value");
-    var FilteredData = tableData.filter(sighting => {
-        sighting.datetime === inputDate
-        sighting.city === inputCity
-    });
-    tbody.html("");
+    var inputState = inputFilterState.property("value");
+    var FilteredData = tableData;
+    if (inputDate) {
+        FilteredData = FilteredData.filter(sighting => sighting.datetime === inputDate)
+    }
+    if (inputCity) {
+        FilteredData = FilteredData.filter(sighting => sighting.city === inputCity)
+    }
+    if(inputState) {
+        FilteredData = FilteredData.filter
+    }
+
+    
     FilteredData.forEach((UFOReport) => {
         var row = tbody.append("tr");
         Object.entries(UFOReport).forEach(([key, value]) => {
@@ -37,3 +51,4 @@ function runFilter() {
         });
     });
 };
+
